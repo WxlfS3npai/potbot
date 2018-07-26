87cb2ffdffd490ea88a09f1e35cbbe1d14501b1e
WxlfS3npai <40581069+WxlfS3npai@users.noreply.github.com>
2018-07-26 19:12:45 +0200

    Update pottbot.py

--- pottbot.py
+++ pottbot.py
@@ -3,6 +3,10 @@ from discord.ext import commands
 import asyncio
 import os
 client = commands.Bot(command_prefix= ',')
+client.remove_command('help')
+async def test():
+    print('test')
+    ...
 
 @client.event
 async def on_ready():
